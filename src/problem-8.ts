a9195deca98ff53cf97820a77350fb1e09aaa259
tuheen66 <hassan.monirul@gmail.com>
2024-11-08 04:57:23 +0600

    updated problem 8

--- src/problem-8.ts
+++ src/problem-8.ts
@@ -1,6 +1,6 @@
 {
   //
-  function validateKeys(obj: T, keys: (keyof T)[]): boolean {
+  function validateKeys<T extends object>(obj: T, keys: (keyof T)[]): boolean {
     for (let key of keys) {
       if (!(key in obj)) {
         return false;
